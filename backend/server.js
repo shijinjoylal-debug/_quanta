ba291bf63610b98a84b8421bab6b5bd5a9d8a126
Shirin Joylal <joylalshirin@gmail.com>
2026-03-03 09:32:34 +0530

    changed url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Adjust based on where your frontend runs
+    origin: ['https://quanta-khaki.vercel.app/'], // Adjust based on where your frontend runs
     credentials: true
 }));
 app.use(express.json());
@@ -33,7 +33,7 @@ app.use(session({
         collectionName: 'sessions'
     }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // 1 day
+        maxAge: 1000 * 60 * 60 * 24 * 31// 31 day
     }
 }));
 
